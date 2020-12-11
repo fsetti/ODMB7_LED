0b0e72674e590b006e0e9f94e4331a8b936a2c6e
Francesco Setti <fsetti@higgs.physics.ucsb.edu>
2020-12-11 12:01:08 -0800

    update correct FPGA xcku035

--- load.sh
+++ load.sh
@@ -1,3 +1,3 @@
-cp -r ../../ODMB/LED_ODMB7/TEST/LED_test_v2 .
-cp -r ../../ODMB/LED_ODMB7/TEST/LED_test_v2/LED_test_v2.srcs/constrs_1/imports/new/LED_constraints.xdc .
-cp -r ../../ODMB/LED_ODMB7/TEST/LED_test_v2/LED_test_v2.srcs/sources_1/imports/new/LED_blinker.vhd .
+cp -r /data/fsetti/led_xcku035/led_test/* .
+cp ../../ODMB/LED_ODMB7/TEST/LED_test_v2/LED_test_v2.srcs/constrs_1/imports/new/LED_constraints.xdc .
+cp ../../ODMB/LED_ODMB7/TEST/LED_test_v2/LED_test_v2.srcs/sources_1/imports/new/LED_blinker.vhd .
